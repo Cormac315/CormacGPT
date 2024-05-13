4a1d6912955c35b01f9913810bbe6a83031fecb3
Cormac315 <129538275+Cormac315@users.noreply.github.com>
2024-05-13 19:14:46 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "Cormac's GPT3.5",
+  title: "Cormac's GPT",
   description: "欢迎使用",
   viewport: {
     width: "device-width",
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "Cormac's GPT3.5",
+    title: "Cormac's GPT",
     statusBarStyle: "default",
   },
 };
