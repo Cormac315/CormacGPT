dd6c9285cca027f6272bd029356caf501734aab1
Cormac315 <129538275+Cormac315@users.noreply.github.com>
2024-05-13 17:57:19 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Cormac的GPT镜像站
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          输入密码以免费使用GPT-3.5
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
